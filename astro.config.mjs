e1f0ffa2fb5835e7e8f804a7f0134ae25d236edb
Suwan Aryal <aryalsuwan.2003@gmail.com>
2025-11-04 17:28:38 -0600

    add netlify adapter to fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // Server mode for API routes
+  adapter: netlify(),
   publicDir: 'public',
   build: {
     assets: 'assets'
